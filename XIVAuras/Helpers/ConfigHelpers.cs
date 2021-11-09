045705174c783e2e5c0df3583f4359c1ead9977f
Lichie <lichie567@protonmail.com>
2021-11-08 20:11:00 -0800

    change TypeNameHandling back to All, auto breaks importing for some reason

--- XIVAuras/Helpers/ConfigHelpers.cs
+++ XIVAuras/Helpers/ConfigHelpers.cs
@@ -18,7 +18,7 @@ namespace XIVAuras.Helpers
         private static readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
         {
             TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
-            TypeNameHandling = TypeNameHandling.Auto,
+            TypeNameHandling = TypeNameHandling.All,
             SerializationBinder = new XIVAurasSerializationBinder()
         };
 
