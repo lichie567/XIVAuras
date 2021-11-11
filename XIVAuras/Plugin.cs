2b238782cd5a01808ccb169577ff7159fe3a13d9
Lichie <lichie567@protonmail.com>
2021-11-11 01:39:08 -0800

    Update to 0.1.2.0

--- XIVAuras/Plugin.cs
+++ XIVAuras/Plugin.cs
@@ -19,7 +19,7 @@ namespace XIVAuras
 {
     public class Plugin : IDalamudPlugin
     {
-        public static string Version { get; private set; } = "0.1.1.2";
+        public static string Version { get; private set; } = "0.1.2.0";
 
         public string Name => "XIVAuras";
 
